aedf6dc58ebbfc7c5176fdccc161a2465be0f672
Wheat <wheat8989@gmail.com>
2026-01-25 17:25:04 +0800

    init

---
+++ MySwiftUICodingProject/MySwiftUICodingProject/MySwiftUICodingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUICodingProjectApp.swift
+//  MySwiftUICodingProject
+//
+//  Created by wheat on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUICodingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
