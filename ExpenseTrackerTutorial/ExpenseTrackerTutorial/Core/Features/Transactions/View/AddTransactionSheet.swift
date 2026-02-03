4a154205b3ffd5e03ec720212adea05b9ded9d48
Wheat <wheat8989@gmail.com>
2026-02-03 23:30:59 +0800

    feat(Transactions): 添加交易功能的基础视图组件
    新增 AddTransactionSheet 和 CategoryIcon 两个基础视图组件，
    为交易功能模块提供UI结构支持。

---
+++ ExpenseTrackerTutorial/ExpenseTrackerTutorial/Core/Features/Transactions/View/AddTransactionSheet.swift
@@ -0,0 +1,18 @@
+//
+//  AddTransactionSheet.swift
+//  ExpenseTrackerTutorial
+//
+//  Created by wheat on 2/3/26.
+//
+
+import SwiftUI
+
+struct AddTransactionSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTransactionSheet()
+}
---
+++ ExpenseTrackerTutorial/ExpenseTrackerTutorial/Core/Features/Transactions/View/CategoryIcon.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryIcon.swift
+//  ExpenseTrackerTutorial
+//
+//  Created by wheat on 2/3/26.
+//
+
+import SwiftUI
+
+struct CategoryIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryIcon()
+}
